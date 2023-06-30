586d676371bd22fc8b565157079d74740503a394
Emre Açar <ea517645@gmail.com>
2023-06-30 14:57:04 +0300

    Site became responsive for phones.

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { useSelector } from "react-redux";
 function App() {
   const billGatesMoney = useSelector((state) => state.products.billGatesMoney);
   return (
-    <main className="bg-gray-100 px-10 sm:px-3 xl:px-36 py-3 sm:py-5 ">
+    <main className="bg-gray-100 px-3 sm:px-10  xl:px-36 py-3 sm:py-5 ">
       <Header />
       <div className="bg-green-500 rounded-md my-3 sm:my-5 text-white font-semibold py-5 text-center w-full  text-3xl sticky z-1 top-0">
         ${billGatesMoney.toLocaleString()}
--- src/components/Fee.jsx
+++ src/components/Fee.jsx
@@ -14,17 +14,17 @@ const Fee = () => {
   return (
     <section className="bg-white">
       {feeItems[0] && (
-        <div className="py-5 flex flex-col items-center justify-center">
-          <h2 className="text-3xl font-semibold">Your Receipt</h2>
+        <div className="py-5 flex flex-col items-center justify-center px-3">
+          <h2 className="text-2xl sm:text-3xl font-semibold">Your Receipt</h2>
           <div className="py-7 ">
             {feeItems.map((item) => (
               <div
                 key={item.name}
-                className="grid grid-cols-3 text-lg gap-x-2 font-semibold"
+                className="grid sm:grid-cols-3 grid-cols-7 text-lg gap-x-2 font-semibold"
               >
-                <div className="">{item.name}</div>
-                <div>x{item.number}</div>
-                <div className="text-green-600">
+                <div className="sm:col-span-1 col-span-2 sm:text-lg text-[16px]">{item.name}</div>
+                <div className="col-span-1 sm:text-lg text-[16px]">x{item.number}</div>
+                <div className="text-green-600 sm:col-span-1 col-span-4 sm:text-lg text-[16px] text-right">
                   {(
                     Number(item.price.replace(/[,]/g, "").split("$")[1]) *
                     item.number
@@ -34,10 +34,10 @@ const Fee = () => {
               </div>
             ))}
             <div className="bg-black w-full mt-7 h-[1px]"></div>
-            <div className="grid grid-cols-3 pt-3">
-              <div className="font-semibold text-lg">Total</div>
-              <div className="font-semibold text-lg w-full text-right"></div>
-              <div className="font-semibold text-lg w-full text-right text-green-600">
+            <div className="grid sm:grid-cols-3 grid-cols-7 pt-3">
+              <div className="font-semibold sm:text-lg text-[16px] sm:col-span-1 col-span-2">Total</div>
+              {/* <div className="font-semibold text-lg w-full text-right col-span-1"></div> */}
+              <div className="text-right font-semibold sm:text-lg text-[16px] w-full text-green-600 sm:col-span-1 col-span-5">
                 {total.toLocaleString()}$
               </div>
             </div>
